a177f2de5327cc96a7ee4987239d5011592e8fb8
wilxsv <wixl.sv@gmail.com>
2016-06-09 13:09:31 -0600

    avance de vista de establecimientos

--- symfony/src/Sinam/CoreBundle/Controller/PorEstablecimientoController.php
+++ symfony/src/Sinam/CoreBundle/Controller/PorEstablecimientoController.php
@@ -18,29 +18,18 @@ class PorEstablecimientoController extends Controller
 {
     public function indexAction(Request $request)
     {
-		$options = new FarmCatalogoproductos();
-		$form = $this->createForm(new BusquedaEstablecimientoType($this->getDoctrine()->getManager()), $options);
-		$form2 = $this->createForm('Sinam\CoreBundle\Form\BusquedaBasicaType');
-		$form3 = $this->createForm('Sinam\CoreBundle\Form\BusquedaHistorialType');
-		$rest = false;
-		$form->handleRequest($request);
-		if ($form->isValid()) {
-			$repository = $this->getDoctrine()->getRepository('SinamCoreBundle:FarmCatalogoproductos');
-			$query = $repository->createQueryBuilder('p')->where("p.nombre LIKE :nombre")->setParameter('nombre', "%".$form->get('nombre')->getData()."%")->getQuery();
-			$rest = $query->getResult();
-        }
- 		$repository = $this->getDoctrine()->getRepository('SinamCoreBundle:FarmCatalogoproductos');
-    	$query = $repository->createQueryBuilder('p')
-    	->where("p.presentacion != ''")
-		->groupBy('p.presentacion, p.id')
-		->orderBy('p.presentacion', 'ASC')->getQuery();
-		$rest = $query->getResult();
+		$form = $form = $this->createForm('Sinam\CoreBundle\Form\BusquedaEstablecimientoType');
 
-		$repository = $this->getDoctrine()->getRepository('SinamCoreBundle:CtlEstablecimiento');
-		$query = $repository->createQueryBuilder('e')->where("e.latitud <> 0 AND e.longitud <> 0 AND e.idTipoEstablecimiento = 4")->getQuery();
-		$estab = $query->getResult();
+        $em = $this->getDoctrine()->getManager();
+        $farmCatalogoproductos = $em->getRepository('SinamCoreBundle:SabCatCatalogoproductos')->findAll();
+        $repository = $this->getDoctrine()->getRepository('SinamCoreBundle:CtlMunicipio');
+        $ctlMunicipios = $repository->createQueryBuilder('p')->select('p.id, p.nombre')->addSelect('d.id AS depto')
+        	->innerJoin('p.idDepartamento', 'd')->where('p.idDepartamento = d.id')->getQuery()->getResult();
+        $repository = $this->getDoctrine()->getRepository('SinamCoreBundle:CtlEstablecimiento');
+        $CtlEstablecimiento = $repository->createQueryBuilder('e')->select('e.id, e.nombre')->addSelect('m.id AS idMunicipio')
+        	->innerJoin('e.idMunicipio', 'm')->where('e.idMunicipio = m.id')->getQuery()->getResult();
 
         
-		return $this->render('SinamCoreBundle:Consulta:alternativo.html.twig', array( 'form' => $form->createView(), 'form2' => $form2->createView(), 'form3' => $form3->createView(), 'rest'=>$rest, 'select' => $query->getResult(), 'estab' => $estab ));
+		return $this->render('SinamCoreBundle:Consulta:establecimiento.html.twig', array( 'form' => $form->createView(), 'todos' => $farmCatalogoproductos, 'muni' => $ctlMunicipios, 'establecimiento' => $CtlEstablecimiento ));
     }
 }
--- symfony/src/Sinam/CoreBundle/Form/BusquedaEstablecimientoType.php
+++ symfony/src/Sinam/CoreBundle/Form/BusquedaEstablecimientoType.php
@@ -2,12 +2,6 @@
 
 namespace Sinam\CoreBundle\Form;
 
-use Doctrine\ORM\EntityManager;
-use Sinam\CoreBundle\Entity\CtlDepartamento;
-use Sinam\CoreBundle\Entity\CtlMunicipio;
-use Sinam\CoreBundle\Entity\CtlEstablecimiento;
-use Doctrine\ORM\EntityRepository;
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
@@ -16,14 +10,11 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManager;
+
 class BusquedaEstablecimientoType extends AbstractType
 {
-    protected $em;
-
-    function __construct(EntityManager $em)
-    {
-        $this->em = $em;
-    }
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
@@ -32,77 +23,45 @@ class BusquedaEstablecimientoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nombre',  'text', array('label' => 'Nombre del medicamento','attr'=>  array('size' => '')))
+            ->add('nombre',  'entity', array('label' => 'Nombre del medicamento','attr'=>  array('style' => 'width: 80%'), 'class' => 'SinamCoreBundle:SabCatCatalogoproductos','property'=>'nombre',
+                'choices_as_values' => true,
+                'query_builder' => function(EntityRepository $er) {
+                    return $er->createQueryBuilder('p')
+                    ->where('p.idTipoproducto = 1')
+                    ->orderBy('p.nombre', 'ASC');
+                },
+            ))
+            ->add('lugar',  'text', array('label' => 'Nombre del medicamento','attr'=>  array('size' => '90%')))
+            ->add('limite',  'number', array('label' => 'Numero de establecimientos a mostrar','attr'=>  array('size' => '90%')))
+            ->add('departamento',  'entity', array('label' => 'departamento','attr'=>  array('onchange' => 'cargarMun(this.value);', 'style' => 'width: 90%'), 'class' => 'SinamCoreBundle:CtlDepartamento','property'=>'nombre',
+                'choices_as_values' => true,
+                'query_builder' => function(EntityRepository $er) {
+                    return $er->createQueryBuilder('p')
+                    ->where('p.idPais = 68')
+                    ->orderBy('p.nombre', 'ASC');
+                },
+            ))
+            ->add('municipio',  'entity', array('label' => 'municipio','attr'=>  array('onchange' => 'cargarEsta(this.value);', 'style' => 'width: 90%'), 'class' => 'SinamCoreBundle:CtlMunicipio','property'=>'nombre',
+                'choices_as_values' => true,
+                'query_builder' => function(EntityRepository $er) {
+                    return $er->createQueryBuilder('p')
+                    ->orderBy('p.nombre', 'ASC');
+                },
+            ))
+            ->add('establecimiento', 'entity', array('label' => 'establecimiento','attr'=>  array('style' => 'width: 90%'), 'class' => 'SinamCoreBundle:CtlEstablecimiento','property'=>'nombre', 'choices_as_values' => true,
+                'query_builder' => function(EntityRepository $er) { return $er->createQueryBuilder('p')->orderBy('p.nombre', 'ASC'); }, ))
+            ->add('save', 'submit', array('label' => 'Buscar','attr'=>  array('class' => 'submit-btn')));
+        ;
+        $builder
+            
             ->add('tipo', 'hidden', array( 'data' => 'establecimiento',))
             ->add('save', 'submit', array('label' => 'Buscar','attr'=>  array('class' => 'submit-btn')));
         //$builder->addEventListener(FormEvents::PRE_SET_DATA, array($this, 'onPreSetData'));
         //$builder->addEventListener(FormEvents::PRE_SUBMIT, array($this, 'onPreSubmit'));
     }
-    
-    protected function addElements(FormInterface $form, $depto = null, $munis = null) {
-		
-		
-        // Remove the submit button, we will place this at the end of the form later
-        $submit = $form->get('save');
-        $form->remove('save');
 
-
-        // Add the province element
-        $form->add('departamento', 'entity', array(
-            'data' => $depto,
-            'empty_value' => '-- Seleccionar --',
-            'class' => 'SinamCoreBundle:CtlDepartamento',
-            'query_builder' => function(EntityRepository $repository) { 
-            return $repository->createQueryBuilder('u')->orderBy('u.idPais, u.nombre', 'ASC');
-        },
-            'mapped' => false)
-        );
-        
-        $municipios = array();
-        if ($depto) {
-            $repo = $this->em->getRepository('SinamCoreBundle:CtlMunicipio');
-            $municipios = $repo->findByNombre($depto, array('nombre' => 'asc'));
-        }
-        $establecimientos = array();
-        if ($depto) {
-            $repo = $this->em->getRepository('SinamCoreBundle:CtlEstablecimiento');
-            $municipios = $repo->findByNombre($depto, array('nombre' => 'asc'));
-        }
-
-        $form->add('municipio', 'entity', array(
-            'empty_value' => '-- Seleccione departamento primero --',
-            'class' => 'SinamCoreBundle:CtlMunicipio',
-            'choices' => $municipios,
-        ));
-        
-        $form->add('establecimiento', 'entity', array(
-            'empty_value' => '-- Seleccione municipio primero --',
-            'class' => 'SinamCoreBundle:CtlEstablecimiento',
-            'choices' => $establecimientos,
-        ));
-
-        // Add submit button again, this time, it's back at the end of the form
-        $form->add($submit);
-    }
-    
-     function onPreSubmit(FormEvent $event) {
-        $form = $event->getForm();
-        $data = $event->getData();
-
-        // Note that the data is not yet hydrated into the entity.
-        $municipio = $this->em->getRepository('SinamCoreBundle:CtlMunicipio')->find($data['municipio']);
-        $establecimientos = $this->em->getRepository('SinamCoreBundle:CtlEstablecimiento')->find($data['establecimiento']);
-        $this->addElements($form, $municipio, $establecimientos);
-    }
-    
-    function onPreSetData(FormEvent $event) {
-        $account = $event->getData();
-        $form = $event->getForm();
-        $this->addElements($form, null);
-    }
-    
     public function getName()
     {
-        return "establecimiento_type";
+        return "establecimiento";
     }
 }
